92b0c5b13bfe343dde8db96e5ac22fd0202488c0
Elton Mwale <ekhaemba1996@gmail.com>
2020-10-12 23:26:43 -0400

    Refactor function names; Created get pokemon by name endpoint

---
+++ api/get_pokemon_by_name.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+import csv
+import os
+import decimal
+
+# https://stackoverflow.com/questions/16957275/python-to-json-serialization-fails-on-decimal
+# For Serializing Decimal Objects returned from DDB
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
+# Dynamo client
+dynamodb = boto3.resource('dynamodb')
+
+def handle(event, context):
+    # Key and Bucket Name from environment variables
+    table_name = os.environ['TABLE_NAME']
+    
+    table = dynamodb.Table(table_name)
+    pokemon_name = event['pathParameters']['name']
+    kargs = {
+        'IndexName': 'NameIndex',
+        'KeyConditionExpression': Key('Name').eq(pokemon_name)
+    }
+    query_result = table.query(
+        **kargs
+    )
+    body = {
+        "result": query_result["Items"]
+    }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body, default=decimal_default)
+    }
+
+    return response
--- data-ingest.py
+++ data-ingest.py
@@ -40,12 +40,3 @@ def handle(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
